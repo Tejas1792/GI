23a04eda01b0747f3bf397aa5bd351029b8d6597
Tejas <171192tejaspatil@gmail.com>
2021-06-09 11:26:00 +0530

    Hello

---
+++ GIAssignment/Login.aspx
@@ -0,0 +1,164 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="GIAssignment.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Login/SignUp</title>
+    <style>
+        #divMain {
+            background-color: #cccccc;
+            width: 30%;
+            height: 315px;
+            text-align: center;
+            font-family: Arial;
+        }
+
+        table {
+            height: 250px;
+        }
+
+        #divlogin {
+            background-color: #e6e6e6;
+        }
+
+        td {
+            text-align: left;
+            padding: 2px;
+        }
+
+        divMain, divlogin, td {
+            font-family: Arial;
+            font-size: small;
+        }
+
+        .button {
+            background-color: #cccccc;
+            color: black;
+            border: 2px solid #e6e6e6;
+            padding: 6px 12px;
+            text-align: center;
+            display: inline-block;
+            cursor: pointer;
+        }
+
+            .button:hover {
+                background-color: black;
+                color: white;
+            }
+
+        .linkbutton:link, .linkbutton:visited {
+            background-color: #cccccc;
+            color: black;
+            border: 2px solid #e6e6e6;
+            padding: 6px 14px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-family: Arial;
+        }
+
+        .linkbutton:hover, .linkbutton:active {
+            background-color: black;
+            color: white;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div id="divMain" runat="server">
+            <asp:Label ID="lblDisplay" runat="server" Text="GIAssignment Sign-In/ Sign-up"></asp:Label>
+            <br />
+            <br />
+
+            <div id="divlogin" runat="server">
+                <table>
+                    <tr id="trddloptions" runat="server">
+                        <td>
+                            <asp:Label ID="lblSelectoption" runat="server" Text="Select Option"></asp:Label>
+                        </td>
+                        <td>
+                            <asp:LinkButton ID="lnklogin" runat="server" OnClick="lnklogin_Click" class="linkbutton">Login</asp:LinkButton>
+
+                            &nbsp;/
+                            <asp:LinkButton ID="lnksignup" runat="server" OnClick="lnksignup_Click" class="linkbutton">Sign Up</asp:LinkButton>
+
+                        </td>
+
+                    </tr>
+                    <tr id="trphone" runat="server">
+                        <td>
+                            <asp:Label ID="lblCellNo" runat="server" Text="Your Mobile Number"></asp:Label>
+                        </td>
+                        <td>
+                            <asp:TextBox ID="txtCellNo" runat="server" Height="20px"></asp:TextBox>
+                            &nbsp;<br />
+                            &quot;Format +91xxxxxxxxxx&quot;</td>
+                    </tr>
+                    <tr id="trsendotp" runat="server">
+                        <td></td>
+                        <td>
+                            <%--text changes after requesting OTP--%>
+                            <asp:Button ID="btngetOTP" runat="server" Text="Get OTP" OnClick="btngetOTP_Click" class="button" />
+                            &nbsp;<asp:Label ID="lblOTPmessage1" runat="server" Text=""></asp:Label>
+                        </td>
+                    </tr>
+                    <tr id="trreceiveotp" runat="server">
+                        <td>
+                            <asp:Label ID="lblDisplayOTP" runat="server" Text="Enter OTP"></asp:Label>
+
+
+                        </td>
+                        <td>
+                            <asp:TextBox ID="txtOTP" runat="server" Height="20px"></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr id="trverifyotp" runat="server">
+                        <td>
+                            
+                        </td>
+                        <td>
+
+                            <asp:Button ID="btnOTPVerify" runat="server" Text="Verify OTP" OnClick="btnOTPVerify_Click" class="button" />
+
+                            &nbsp;
+                            <asp:Label ID="lblOTPmessage" runat="server" Text=""></asp:Label>
+
+                        </td>
+                    </tr>
+                    <tr id="tremailid" runat="server">
+                        <td>
+                            <asp:Label ID="lblEmail" runat="server" Text="Enter your E-Mail:"></asp:Label>
+                        </td>
+                        <td>
+                            <asp:TextBox ID="txtEmail" runat="server" Height="20px"></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr id="trname" runat="server">
+                        <td>
+                            <asp:Label ID="lblName" runat="server" Text="Enter your Name:"></asp:Label>
+                        </td>
+                        <td>
+                            <asp:TextBox ID="txtName" runat="server" Height="20px"></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr id="trregister" runat="server">
+                        <td></td>
+                        <td>
+
+                            <asp:Button ID="btnregister" runat="server" Text="Register Me" OnClick="btnregister_Click" class="button" />
+
+                        </td>
+                    </tr>
+                    <tr id="trdiaplaymessage" runat="server">
+                        <td>&nbsp;</td>
+                        <td>
+                            <asp:Label ID="lblMessage" runat="server" Text=""></asp:Label>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ GIAssignment/Login.aspx.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Text;
+using RestSharp;
+using System.Collections.Generic;
+using System.Net;
+using System.Collections.Specialized;
+using System.IO;
+using System.Data;
+using System.Configuration;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Data.SqlClient;
+
+namespace GIAssignment
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        DataSet ds = new DataSet();
+        SqlConnection con;
+        public static string generatenumber;
+              
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //prevents from loading page on click
+            if (!Page.IsPostBack == true)
+            {
+                trphone.Visible = false;
+                trsendotp.Visible = false;
+                trreceiveotp.Visible = false;
+                trverifyotp.Visible = false;
+                tremailid.Visible = false;
+                trname.Visible = false;
+                trregister.Visible = false;
+            }
+
+        }
+
+        protected void lnklogin_Click(object sender, EventArgs e)
+        {
+            if (trphone.Visible == true)
+            {
+                Page.Response.Redirect(Page.Request.Url.ToString(), true);
+            }
+            else
+            {
+                trphone.Visible = true;
+                trsendotp.Visible = true;
+            }
+
+        }
+
+        public void lnksignup_Click(object sender, EventArgs e)
+        {
+            Session["checklink"] = lnksignup.Text;
+
+            if (trphone.Visible == true)
+            {
+                Page.Response.Redirect(Page.Request.Url.ToString(), true);
+            }
+            else
+            {
+                trphone.Visible = true;
+                trsendotp.Visible = true;
+            }
+
+            if (lblOTPmessage.Text == "Sucessfull")
+            {
+                tremailid.Visible = true;
+                trname.Visible = true;
+                trregister.Visible = true;
+            }
+        }
+
+        protected void btngetOTP_Click(object sender, EventArgs e)
+        {
+            if (txtCellNo.Text == "")
+            {
+                lblOTPmessage1.Text = "Enter your Mobile No.";
+            }
+            else
+            {
+                trreceiveotp.Visible = true;
+                trverifyotp.Visible = true;
+                SendOTP();
+            }
+            
+        }
+
+        protected void btnOTPVerify_Click(object sender, EventArgs e)
+        {
+            VerifyOTP();
+
+            if (Session["checklink"] != null)
+            {
+                tremailid.Visible = true;
+                trname.Visible = true;
+                trregister.Visible = true;
+            }
+            else
+            {
+                Session.Abandon();
+                CheckNoinDB();
+            }
+            
+        }
+
+        protected void btnregister_Click(object sender, EventArgs e)
+        {
+            con = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\64220\OneDrive\Desktop\GI Assignment\GIAssignment\GIAssignment\App_Data\GIAssignment.mdf;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand();
+            
+            try
+            {
+                cmd = new SqlCommand("SPInsert", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                SqlParameter spName = cmd.Parameters.Add("@Name", SqlDbType.VarChar, 50);
+                spName.Value = txtName.Text;
+
+                SqlParameter spEmailID = cmd.Parameters.Add("@EmailID", SqlDbType.Char, 50);
+                spEmailID.Value = txtEmail.Text;
+
+                SqlParameter spPhoneNo = cmd.Parameters.Add("@MobileNo", SqlDbType.Char, 20);
+                spPhoneNo.Value = txtCellNo.Text;
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+
+            }
+
+            catch (Exception)
+            {
+                lblMessage.Text = "Try again";
+            }
+            finally
+            {
+                con.Close();
+                lblMessage.Text = ""+ txtName.Text + " is now registered.";
+            }
+
+        }
+
+        public void SendOTP()
+        {
+            String result;
+            string apiKey = "4103ee04-c476-11eb-8089-0200cd936042";
+            string numbers = txtCellNo.Text;
+            
+            Random rnd = new Random();
+            generatenumber = (rnd.Next(1000, 9999)).ToString();
+            
+            String url = "https://2factor.in/API/V1/" + apiKey + "/SMS/" + numbers + "/" + generatenumber +"/OTP";
+            
+            //refered to third party for API code and url string
+
+            StreamWriter myWriter = null;
+            HttpWebRequest objRequest = (HttpWebRequest)WebRequest.Create(url);
+
+            objRequest.Method = "POST";
+            objRequest.ContentLength = Encoding.UTF8.GetByteCount(url);
+            objRequest.ContentType = "application/x-www-form-urlencoded";
+            try
+            {
+                myWriter = new StreamWriter(objRequest.GetRequestStream());
+                myWriter.Write(url);
+            }
+            catch
+            {
+                lblOTPmessage1.Text = "Enter Valid Contact Number";
+            }
+            finally
+            {
+                myWriter.Close();
+            }
+
+            HttpWebResponse objResponse = (HttpWebResponse)objRequest.GetResponse();
+            using (StreamReader sr = new StreamReader(objResponse.GetResponseStream()))
+            {
+                result = sr.ReadToEnd();
+                sr.Close();
+            }
+
+            lblOTPmessage.Text = "OTP Sent";
+        }
+
+        public void VerifyOTP()
+        {
+            if(txtOTP.Text == generatenumber)
+            {
+                lblOTPmessage.Text = "OTP Verified, Thank you";
+            }
+            else
+            {
+                lblOTPmessage.Text = "Incorrect OTP";
+            }
+            
+        }
+
+        public void CheckNoinDB()
+        {
+            // Enter Your Connectionstring
+            con = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\64220\OneDrive\Desktop\GI Assignment\GIAssignment\GIAssignment\App_Data\GIAssignment.mdf;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand();
+            
+            try
+            {
+                cmd = new SqlCommand("CheckUser", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                SqlParameter spPhoneNo = cmd.Parameters.Add("@MobileNo", SqlDbType.Char, 20); 
+                spPhoneNo.Value = txtCellNo.Text;
+                con.Open();
+
+                //cmd.Parameters["@MobileNo"].Direction = ParameterDirection.Output;
+                var result = cmd.ExecuteScalar();
+                cmd.ExecuteNonQuery();
+                
+                if (result == null)
+                {
+                    lblMessage.Text = "Please Sign-Up for GI Assesment, Thank you";
+                }
+                else
+                {
+                    lblMessage.Text = "You have already signed in, Thank you";
+                }
+            }
+
+            catch (Exception)
+            {
+                lblMessage.Text = "Try again";
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+    }
+}
